b17882a40f577acd323f89d20036294cc015f385
Dima Snisarenko <snisarenkodima@gmail.com>
2020-06-01 18:20:50 +0200

    chore: rename img-comparison-slider-angular to example-img-comparison-slider-angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'img-comparison-slider-angular'`, () => {
+  it(`should have as title 'example-img-comparison-slider-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('img-comparison-slider-angular');
+    expect(app.title).toEqual('example-img-comparison-slider-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('img-comparison-slider-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('example-img-comparison-slider-angular app is running!');
   });
 });
