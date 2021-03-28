52191fcd8d59042baf89c173a0db2642b7293b8a
Dima Snisarenko <snisarenkodima@gmail.com>
2021-03-28 11:01:31 +0200

    feat: dynamically change images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  before = 'https://sneas.github.io/img-comparison-slider/demo/images/before.jpg';
-  after = 'https://sneas.github.io/img-comparison-slider/demo/images/after.jpg';
+  series = 1;
+
+  public changeImages() {
+    this.series = this.series === 1 ? 2 : 1;
+  }
 }
