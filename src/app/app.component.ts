d0511c031b85b6210a24f511e812861aacc688f3
Dima Snisarenko <snisarenkodima@gmail.com>
2021-06-05 10:33:48 +0200

    feat: reset example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HTMLImgComparisonSliderElement } from "img-comparison-slider";
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('slider') slider: ElementRef<HTMLImgComparisonSliderElement>;
+
   series = 1;
 
   public changeImages() {
     this.series = this.series === 1 ? 2 : 1;
+    this.slider.nativeElement.reset();
   }
 }
